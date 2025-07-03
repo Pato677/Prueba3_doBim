ae30b4fa70fa098507a6fffca2becc283f199405
JORDY PATRICIO MAYORGA NUNEZ <jordy.mayorga@epn.edu.ec>
2025-07-03 15:02:51 -0500

    m

--- prueba/src/Componentes/Inicio.jsx
+++ prueba/src/Componentes/Inicio.jsx
@@ -7,10 +7,10 @@ const Inicio = () => {
       <h1>Bienvenido a la Aplicación Emaseo</h1>
       <p>Esta es una aplicación nos permitira un sistema de registro de los puntos de recolección.</p>
         <div>
-            <Link to="/crear-puntos" className="btn btn-primary">
+            <Link to="/CrearPuntos" className="btn btn-primary">
                 Ir a Crear Puntos
             </Link>
-            <Link to="/ver-puntos" className="btn btn-secondary ml-2">
+            <Link to="/ListaPuntos" className="btn btn-secondary ml-2">
                 Ver Puntos
             </Link>
             <Link to="/usuarios" className="btn btn-success ml-2">
--- prueba/src/Componentes/ListaPuntos.jsx
+++ prueba/src/Componentes/ListaPuntos.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Punto from './punto';
+import Punto from './Punto';
 import './estilos/ListaPuntos.css';
 import { Link } from "react-router-dom";
 
